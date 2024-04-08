dfed71bb2229e686f38859a59bb202b459fc0d44
RitikaRai2003 <111782346+RitikaRai2003@users.noreply.github.com>
2024-04-08 16:31:00 +0000

    comparision

---
+++ o1_basic/04_comparision.js
@@ -0,0 +1,22 @@
+//console.log(2 > 1);
+//console.log(2 <= 1);
+//console.log(2 < 1);
+//console.log(2 ==1)
+//console.log(2 != 1);
+
+//comparision of different datatype
+//console.log("2"> 1);
+//console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 1);
+
+
+console.log(undefined == 0);//it gives false output in all cases == < >
+
+//strik check(===) in this its check datatype as well value
+
+console.log("2 " === 2);
+
+

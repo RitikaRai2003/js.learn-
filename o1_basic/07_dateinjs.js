aa82842b61dad1a2462d833e7b7d5a8b8099a134
RitikaRai2003 <111782346+RitikaRai2003@users.noreply.github.com>
2024-04-18 12:21:13 +0000

    dateinjs

---
+++ o1_basic/07_dateinjs.js
@@ -0,0 +1,26 @@
+//Dates
+
+let myDate = new Date()
+//console.log(typeof myDate);
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toJSON());
+//console.log(myDate.toLocaleDateString());
+//console.log(myDate.toLocaleString());
+//console.log(myDate.toLocaleTimeString());
+//console.log(myDate.toTimeString());
+//console.log(myDate.toUTCString());
+//console.log(myDate.getTimezoneOffset());
+
+//let myCreatedDate = new Date(2024, 0, 23)
+//console.log(myCreatedDate.toDateString());
+
+//let myCreatedDate = new Date(2024, 0, 23 , 5, 3)
+//let myCreatedDate =  new Date("2024-01-14")
+let myCreatedDate =  new Date("01-14-2024")
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);

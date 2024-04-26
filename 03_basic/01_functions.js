cd7b9250a5eacac0b362ffba9735a91968bf8072
RitikaRai2003 <111782346+RitikaRai2003@users.noreply.github.com>
2024-04-26 21:20:53 +0000

    function

---
+++ 03_basic/01_functions.js
@@ -0,0 +1,12 @@
+function sayMyName(){
+    console.log("h");
+    console.log("i");
+    console.log("k");
+    console.log("m");
+}
+sayMyName()
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+
+}

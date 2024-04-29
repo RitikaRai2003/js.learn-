39c232a7cbbd67d5856efd0b73fcc1dec1bcb89f
RitikaRai2003 <111782346+RitikaRai2003@users.noreply.github.com>
2024-04-29 18:35:50 +0000

    scope

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -10,3 +10,43 @@ if(true){//block scope
 console.log(a);//400
 //console.log(b);
 //console.log(c);
+
+function one(){
+    const  username = "ritika"
+    
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+
+    //console.log(website);  error bcz scope se outside  ha
+    two()
+}
+ one()
+
+ if(true){
+    const username = "ritika"
+    if(username === "ritika"){
+        const website = "youtube"
+        console.log(username + website);
+    }
+    //console.log(website); error bcz call outside the scope
+ }
+//console.log(username); scope nhi ha
+
+
+//================interesting ==========
+
+console.log(addone(5))// no error bcz we can call function before declare function
+function addone(num){
+    return num + 1
+
+}
+
+//not call before the function declare
+//addTwo(5) display error bcz its fuction hold in a variable 
+const addTwo = function(num){
+    return num +2
+}
+
+addTwo(5) 

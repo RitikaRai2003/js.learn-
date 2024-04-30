0b6c95e9dda92b962629997c4ebb1127c9c80a15
RitikaRai2003 <111782346+RitikaRai2003@users.noreply.github.com>
2024-04-30 18:48:39 +0000

    control flow

---
+++ 04_controlflow/one.js
@@ -0,0 +1,11 @@
+//if 
+const isUserloggedIn = true
+const temperature = 41
+
+if(gtemperature < 50){
+    console.log("excuted")
+
+}
+
+/* comparsion operator  
+ < ,> , <= ,>= , == ,!= , === (equal and tpye check)*/

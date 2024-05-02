78f7dcfe4f4f563a25d5167009d6092d771986a8
RitikaRai2003 <111782346+RitikaRai2003@users.noreply.github.com>
2024-05-02 14:33:59 +0000

    truthy and falsy value

--- 04_controlflow/truthy.js
+++ 04_controlflow/truthy.js
@@ -18,3 +18,25 @@ if(userEmail.length ===0){
     console.log("array is empty")
 }
 
+//check for object 
+const emptyObj = {}  
+ if(Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+ }
+
+
+ //Nullish Coalescing operator (??) : null undefined
+
+ let val1;
+ //val1 =  5 ?? 10
+ val1  = null ?? 10
+//val1 = undefined ?? 15
+//val1 = null ?? 10 ?? 11
+
+ console.log(val1);
+
+ //Terniary Operator:-
+//condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80"):console.log("more than 80")

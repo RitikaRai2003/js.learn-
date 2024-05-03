8768e8863eaa4bb7ec7be020ca0d19746b3e3561
RitikaRai2003 <111782346+RitikaRai2003@users.noreply.github.com>
2024-05-03 11:38:12 +0000

    for in loop

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -1,10 +1,28 @@
-const mObject = {
+const myObject = {
     js: 'javascript',
     cpp:'c++',
     rb:'ruby',
     swift: 'swift by apple'
 }
 
-for (const key in object) {
-    i
+for (const key in myObject) {
+    //console.log(myObject[key]);
+    //console.log(`${key} shortcut for ${myObject[key]}`);
+}
+
+const programming = [ "js", "c++", "ruby"]
+
+for (const key in programming) {
+    //console.log(programming[key])
+    
+}
+
+const map = new Map()
+map.set('IN', "india")
+map.set('USA', "united states of America")
+map.set('Fr', "France")
+map.set('IN', "india")
+
+for (const key in map) {
+    console.log(key)// map is not iterative so its not executed
 }
